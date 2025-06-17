a66f0103a132405d5c729ed6da98cc339cd21ba3
Avinash Pathak <avipathak732@gmail.com>
2025-06-17 15:21:12 +0530

    Implement Component Library

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
+import CompanionCard from "@/components/CompanionCard"
+import SearchInput from "@/components/SearchInput"
+import SubjectFilter from "@/components/SubjectFilter"
+import { getAllCompanion } from "@/lib/actions/companion.actions"
+import { getSubjectColor } from "@/lib/utils"
 
-const CompanionsLibrary= () => {
+const CompanionsLibrary= async ({searchParams}: SearchParams) => {
+  const filters = await searchParams
+  const subject = filters.subject ? filters.subject : ''
+  const topic = filters.topic ? filters.topic : ''
+
+  const companions = await getAllCompanion({subject, topic})
+
   return (
-    <div>
-      <h1 className='text-2xl underline'>Learning Companions Page</h1>
-    </div>
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+        </div>
+      </section>
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard 
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+
+      </section>
+    </main>
   )
 }
 
